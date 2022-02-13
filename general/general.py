42948ce3f9984f28ea544130231179adab0dfe25
Jordi Gilabert <jordi.gilabert@typeform.com>
2022-02-13 12:45:56 +0100

    First commit

---
+++ general/general.py
@@ -0,0 +1,14 @@
+ifrom datetime import dateteim
+
+
+def isotoepoch(ts):
+
+    return int(datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%f").timestamp())
+
+
+def epochtoiso(t):
+
+    return datetime.utcfromtimestamp(t).isoformat()
+
+
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+  name='tfsecutils',
+  version='1.0',
+  install_requires=[]
+)
+
